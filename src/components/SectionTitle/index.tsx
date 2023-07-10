da8d6035c25227cf2338204bc945ca5ce0f3a354
Approximations <guilhermeksilva556@gmail.com>
2023-07-09 21:55:02 -0300

    section title

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLHeadingElement> {
     children: React.ReactNode;
 }
 
-export default function SectionTitle({ children }: Props) {
+export default function SectionTitle({ className, children }: Props) {
     return (
         <h2
-            className="flex items-center gap-2 w-full whitespace-nowrap font-medium before:block before:h-[1px] before:w-full before:bg-lightGray
-        after:block after:h-[1px] after:w-full after:bg-lightGray"
+            className={twMerge(
+                'flex items-center gap-2 w-full whitespace-nowrap font-medium before:block before:h-[1px] before:w-full before:bg-lightGray after:block after:h-[1px] after:w-full after:bg-lightGray text-gray',
+                className
+            )}
         >
             {children}
         </h2>
