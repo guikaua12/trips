564c99f5a7a4825d015ea32ef511a4e44c60b4c4
Approximations <guilhermeksilva556@gmail.com>
2023-07-09 21:54:46 -0300

    added integration with the api to get recommended trips

---
+++ src/app/components/RecommendedTrips/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SectionTitle from '@/components/SectionTitle';
+import { Trip } from '@/types/Trip';
+import TripComponent from '@/components/Trip';
+
+interface RecommendedTripsProps {
+    trips: Trip[];
+}
+
+export default function RecommendedTrips({ trips }: RecommendedTripsProps) {
+    return (
+        <div>
+            <SectionTitle className="mb-3">Destinos Recomendados</SectionTitle>
+            <div className="flex flex-col items-center">
+                {trips.map((trip) => {
+                    return <TripComponent key={trip.id} trip={trip} />;
+                })}
+            </div>
+        </div>
+    );
+}
