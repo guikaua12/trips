8cf9c889fd43026305d6ff015ededbe9ded4ab14
Approximations <guilhermeksilva556@gmail.com>
2023-07-30 23:32:08 -0300

    fix button text not centered

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,14 @@ import { twMerge } from 'tailwind-merge';
 
 export default function Button({ className, children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
-        <button className={twMerge('outline-none rounded-md bg-purple p-2 text-white text-sm font-medium', className)}>
+        <button
+            className={twMerge(
+                'outline-none rounded-md bg-purple p-2 text-white text-sm font-medium text-center flex justify-center',
+                'disabled:bg-darkPurple',
+                className
+            )}
+            {...props}
+        >
             {children}
         </button>
     );
