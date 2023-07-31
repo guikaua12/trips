c8899317ba71bf0ca3d5237e1c24989c3a3001f9
Approximations <guilhermeksilva556@gmail.com>
2023-07-30 22:23:27 -0300

    added error & hook form integration on Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,27 @@
 import React, { InputHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
+import { FaTriangleExclamation } from 'react-icons/fa6';
 
-export default function Input({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    hookFormRegister?: any;
+    error?: string | undefined | null;
+}
+
+export default function Input({ className, hookFormRegister, error, ...props }: InputProps) {
     return (
-        <input
-            {...props}
-            className={twMerge(
-                'border border-lightGray rounded-md p-2 text-sm text-gray outline-none focus:border-purple focus:text-purple',
-                className
-            )}
-        />
+        <div>
+            <div
+                className={twMerge(
+                    'flex border border-lightGray rounded-md focus-within:border-purple focus-within:text-purple outline-none p-2 text-sm text-gray',
+                    className,
+                    error && 'border-red-500 hover:border-red-500 focus:focus-within:border-red-500'
+                )}
+            >
+                <input className="outline-none w-full" {...props} {...hookFormRegister} />
+                {error && <FaTriangleExclamation size={20} className="text-red-500"></FaTriangleExclamation>}
+            </div>
+
+            {error && <span className="text-red-500 text-sm">{error}</span>}
+        </div>
     );
 }
