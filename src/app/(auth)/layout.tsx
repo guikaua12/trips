fc2892b190b79ad864d9b39dd166b5ad30a1a692
Approximations <datkidbosst@gmail.com>
2023-08-03 19:03:24 -0300

    added private route to auth route group again

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import PrivateRoute from '@/app/components/auth/PrivateRoute';
+import { useAuth } from '@/hooks/useAuth';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-    return children;
-    // <PrivateRoute redirectTo="/" condition={!isAuthenticated()}>
-    //
-    // </PrivateRoute>
+    const { isLogged } = useAuth();
+
+    return (
+        <PrivateRoute redirectTo="/" condition={!isLogged}>
+            {children}
+        </PrivateRoute>
+    );
 };
 
 export default AuthLayout;
