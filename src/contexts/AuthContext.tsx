ddb1913e00d0965ff393335a1a4b51ff16eaabdf
Approximations <datkidbosst@gmail.com>
2023-08-04 00:56:36 -0300

    added nookies instead of localStorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ import { createContext, ReactNode, useEffect, useState } from 'react';
 import { api } from '@/services/api';
 import { AxiosError } from 'axios';
 import { User } from '@/types/User';
+import nookies from 'nookies';
 
 type LoginRequestType = {
     email: string;
@@ -42,10 +43,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
     useEffect(() => {
         const verifySession = async (): Promise<void> => {
-            let session: string | null = JSON.parse(localStorage.getItem('trips_session')!);
+            const { trips_session } = nookies.get({});
+            console.log(trips_session);
 
-            if (session) {
-                const response = await api.get(`/session/verify/${session}`);
+            if (trips_session) {
+                const response = await api.get(`/session/verify/${trips_session}`);
                 if (!response.data) return;
 
                 const { user }: SessionVerifyType = response.data;
@@ -61,7 +63,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
     function setUserFn(data: User | null): void {
         setUser(data);
-        localStorage.setItem('trips_user', JSON.stringify(data));
+        nookies.set({}, 'trips_user', JSON.stringify(data), {
+            // 24 hours
+            maxAge: 60 * 60 * 24,
+            path: '/',
+        });
     }
 
     async function login({ email, password }: LoginRequestType): Promise<LoginResponseType> {
@@ -78,9 +84,13 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
             const { user, session, error, message }: LoginResponseType = response.data;
 
-            if (user) {
+            if (user && session) {
                 setUserFn(user);
-                localStorage.setItem('trips_session', JSON.stringify(session));
+                nookies.set({}, 'trips_session', session, {
+                    // 24 hours
+                    maxAge: 60 * 60 * 24,
+                    path: '/',
+                });
             }
 
             return { user, session, error, message };
@@ -97,8 +107,8 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
     function logout(): void {
         setUser(null);
-        localStorage.removeItem('trips_user');
-        localStorage.removeItem('trips_session');
+        nookies.destroy({}, 'trips_user', { path: '/' });
+        nookies.destroy(null, 'trips_session', { path: '/' });
     }
 
     return <AuthContext.Provider value={{ login, logout, user, setUserFn, isLogged }}>{children}</AuthContext.Provider>;
