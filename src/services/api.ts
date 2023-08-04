c789002a86195e6497a92bd9f96309cd7825962e
Approximations <datkidbosst@gmail.com>
2023-08-04 01:15:27 -0300

    remove default interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,10 +5,10 @@ export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
 });
 
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        console.log(error);
-        return error;
-    }
-);
+// api.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         console.log(error);
+//         return error;
+//     }
+// );
