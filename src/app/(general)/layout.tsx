21eaf8d3be0622d63ebb94385c8dc511b29ed259
Approximations <datkidbosst@gmail.com>
2023-08-04 01:13:11 -0300

    added loading component while children of general pages are loading

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,4 +1,6 @@
 import Header from '@/layout/Header';
+import React, { Suspense } from 'react';
+import Image from 'next/image';
 export const metadata = {
     title: 'Home / Trips',
     description: 'Sistema de reserva de viagens',
@@ -10,12 +12,22 @@ export const metadata = {
     ],
 };
 
+function Loading() {
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center gap-4 p-5 animate-pulse">
+            <div className="w-[60] h-[60]">
+                <Image src="/logo-icon.png" width={60} height={60} alt="Logo" />
+            </div>
+        </div>
+    );
+}
+
 const GeneralLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <>
+        <Suspense fallback={<Loading></Loading>}>
             <Header></Header>
             {children}
-        </>
+        </Suspense>
     );
 };
 
