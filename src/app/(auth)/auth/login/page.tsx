5cf79660c14be42c273458ef98a54348b397de58
Approximations <datkidbosst@gmail.com>
2023-08-04 03:16:25 -0300

    remove unused metadata props

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -5,13 +5,6 @@ import { cookies } from 'next/headers';
 
 export const metadata = {
     title: 'Login / Trips',
-    description: 'Sistema de reserva de viagens',
-    authors: [
-        {
-            name: 'Approximations',
-            url: 'https://github.com/guikaua12',
-        },
-    ],
 };
 
 async function verifyAuth(): Promise<boolean> {
