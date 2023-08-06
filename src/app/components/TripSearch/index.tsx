9ed0dcdbbe1de3a1ddbe5f59a09a33df06be4336
Approximations <datkidbosst@gmail.com>
2023-08-06 00:33:59 -0300

    added functionality to the search form

--- src/app/components/TripSearch/index.tsx
+++ src/app/components/TripSearch/index.tsx
@@ -1,24 +1,60 @@
 'use client';
 
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import Input from '@/components/Input';
 import CurrencyInput from '@/components/CurrencyInput';
 import DatePicker from '@/components/DatePicker';
 import Button from '@/components/Button';
+import { searchTrip, TripSearchType } from '@/services/trips';
 
 export default function TripSearch() {
+    const [tripSearch, setTripSearch] = useState<TripSearchType>({
+        startDate: new Date(),
+    });
+
+    function handleDateChange(date: Date) {
+        setTripSearch(({ ...prevState }) => ({ ...prevState, startDate: date }));
+    }
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        setTripSearch(({ ...prevState }) => ({ ...prevState, [e.target.name]: e.target.value }));
+    }
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(tripSearch);
+
+        const trips = await searchTrip({});
+        console.log(trips);
+    }
+
     return (
         <div className="flex flex-col gap-3 p-4 bg-worldMap bg-no-repeat bg-cover">
             <h1 className="font-semibold text-xl">
-                Encontre sua pròxima <span className="text-purple">viagem!</span>
+                Encontre sua próxima <span className="text-purple">viagem!</span>
             </h1>
 
-            <form action="#" className="flex flex-col gap-3">
-                <Input type="text" className="w-full" placeholder="Onde você quer ir?" />
+            <form action="#" className="flex flex-col gap-3" onSubmit={handleSubmit}>
+                <Input
+                    type="text"
+                    name="location"
+                    className="w-full"
+                    placeholder="Onde você quer ir?"
+                    onChange={handleChange}
+                />
                 <div className="flex gap-3">
-                    {/*<Input type="text" placeholder="Primeira data" />*/}
-                    <DatePicker onChange={() => {}} className="w-full h-full" placeholderText="Primeira data" />
-                    <CurrencyInput className="w-full h-full" placeholder="Orçamento" />
+                    <DatePicker
+                        selected={tripSearch.startDate}
+                        onChange={handleDateChange}
+                        className="w-full h-full"
+                        placeholderText="Primeira data"
+                    />
+                    <CurrencyInput
+                        name="budget"
+                        className="w-full h-full"
+                        placeholder="Orçamento"
+                        onChange={handleChange}
+                    />
                 </div>
 
                 <Button>Pesquisar</Button>
