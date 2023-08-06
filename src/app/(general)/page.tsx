f034749133b883273304dc2752a68e11f365f9f5
Approximations <datkidbosst@gmail.com>
2023-08-06 00:35:53 -0300

    - now main page uses axios instead of fetch

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -2,23 +2,27 @@ import TripSearch from '@/app/components/TripSearch';
 import QuickSearch from '@/app/components/QuickSearch';
 import { Trip } from '@/types/Trip';
 import RecommendedTrips from '@/app/components/RecommendedTrips';
+import { api } from '@/services/api';
 
 interface HomeProps {
     trips: Trip[];
 }
 
 async function getData(): Promise<Trip[]> {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/trips/search', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            recommended: true,
-        }),
-    });
+    try {
+        const response = await api.get('/trips/search', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                recommended: true,
+            },
+        });
 
-    return await response.json();
+        return response.data;
+    } catch (err) {
+        return [];
+    }
 }
 
 const delay = (ms: number = 750) => new Promise((resolve) => setTimeout(resolve, ms));
