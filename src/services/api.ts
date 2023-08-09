3e8d1dcfc6b4e10b12a89e869c3cd44983b092a4
Approximations <datkidbosst@gmail.com>
2023-08-09 19:25:15 -0300

    added a request interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,19 @@
 import axios from 'axios';
-import { useAuth } from '@/hooks/useAuth';
+import nookies from 'nookies';
 
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
 });
 
-// api.interceptors.response.use(
-//     (response) => response,
-//     (error) => {
-//         console.log(error);
-//         return error;
-//     }
-// );
+api.interceptors.request.use((config) => {
+    const { trips_token } = nookies.get({});
+    if (trips_token && !config.headers.authorization) {
+        config.headers.authorization = `Bearer ${trips_token}`;
+    }
+
+    if (config.data) {
+        config.headers['Content-Type'] = 'application/json';
+    }
+
+    return config;
+});
