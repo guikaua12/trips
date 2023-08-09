a4633cca6c8ad7dab6c6823655c21ce62a494130
Approximations <datkidbosst@gmail.com>
2023-08-09 19:42:36 -0300

    removed unused metadata export

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,16 +1,8 @@
 import Header from '@/layout/Header';
-import React, { Suspense } from 'react';
-import Image from 'next/image';
+import React from 'react';
 import Footer from '@/layout/Footer';
 export const metadata = {
     title: 'Home / Trips',
-    description: 'Sistema de reserva de viagens',
-    authors: [
-        {
-            name: 'Approximations',
-            url: 'https://github.com/guikaua12',
-        },
-    ],
 };
 
 const GeneralLayout = ({ children }: { children: React.ReactNode }) => {
