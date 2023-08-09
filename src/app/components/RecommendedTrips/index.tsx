8d046d3dc3975624451074a2a85bbd87bb5e94f1
Approximations <datkidbosst@gmail.com>
2023-08-09 19:35:29 -0300

    redirect user trip details page on click

--- src/app/components/RecommendedTrips/index.tsx
+++ src/app/components/RecommendedTrips/index.tsx
@@ -4,18 +4,29 @@ import React from 'react';
 import SectionTitle from '@/components/SectionTitle';
 import { Trip } from '@/types/Trip';
 import TripComponent from '@/components/Trip';
+import { useRouter } from 'next/navigation';
 
 interface RecommendedTripsProps {
     trips: Trip[];
 }
 
 export default function RecommendedTrips({ trips }: RecommendedTripsProps) {
+    const { push } = useRouter();
+
     return (
         <div className="p-4 container m-auto">
             <SectionTitle className="mb-3 m-auto">Destinos Recomendados</SectionTitle>
             <div className="flex flex-col items-center sm:flex-row sm:gap-8 sm:justify-center sm:gap-16">
                 {trips.map((trip) => {
-                    return <TripComponent key={trip.id} trip={trip} />;
+                    return (
+                        <TripComponent
+                            key={trip.id}
+                            trip={trip}
+                            onClick={() => {
+                                push(`/trips/${trip.id}`);
+                            }}
+                        />
+                    );
                 })}
             </div>
         </div>
