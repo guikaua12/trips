e37126ddebc5cdae1ac7da56abc23b2a3a540596
Approximations <datkidbosst@gmail.com>
2023-08-13 19:15:57 -0300

    style: style adjustments on Trip component

--- src/components/Trip/index.tsx
+++ src/components/Trip/index.tsx
@@ -12,32 +12,32 @@ interface Props {
 
 export default function Trip({ trip, href }: Props) {
     return (
-        <div className="flex flex-col mb-5 w-[280px] cursor-pointer" onClick={onClick}>
-            <div className="relative h-[280px] w-full">
-                <Image
-                    src={trip.coverImage}
-                    fill
-                    alt="Cover Image"
-                    style={{
-                        objectFit: 'cover',
-                    }}
-                    className="rounded-2xl"
-                />
-            </div>
         <Link href={href || '#'}>
-
-            <div className="flex flex-col mt-2">
-                <h1 className="text-sm font-medium text-darkPurple mb-1">{trip.name}</h1>
-                <div className="flex gap-1">
-                    <ReactCountryFlag countryCode={trip.countryCode} svg />
-                    <span className="text-xs text-gray">{trip.location}</span>
+            <div className="mb-5 flex w-[280px] cursor-pointer flex-col">
+                <div className="relative h-[280px] w-full">
+                    <Image
+                        src={trip.coverImage}
+                        fill
+                        alt="Cover Image"
+                        style={{
+                            objectFit: 'cover',
+                        }}
+                        className="rounded-2xl"
+                    />
                 </div>
-                <div className="flex gap-1">
-                    <span className="text-xs font-semibold text-purple">R${trip.pricePerDay}</span>{' '}
-                    <span className="text-xs text-gray">por noite</span>
+
+                <div className="mt-2 flex flex-col">
+                    <h1 className="mb-1 text-sm font-medium text-darkPurple">{trip.name}</h1>
+                    <div className="flex gap-1">
+                        <ReactCountryFlag countryCode={trip.countryCode} svg />
+                        <span className="text-xs text-gray">{trip.location}</span>
+                    </div>
+                    <div className="flex gap-1">
+                        <span className="text-xs font-semibold text-purple">R${trip.pricePerDay}</span>{' '}
+                        <span className="text-xs text-gray">por noite</span>
+                    </div>
                 </div>
             </div>
-        </div>
         </Link>
     );
 }
