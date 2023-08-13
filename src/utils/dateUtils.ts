08e3770be63df826ae1aa0b26e1d438deae1b01a
Approximations <datkidbosst@gmail.com>
2023-08-13 19:16:35 -0300

    export isIsoDateString from dateUtils

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -2,7 +2,7 @@ import { parseISO } from 'date-fns';
 
 const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
 
-function isIsoDateString(value: any): boolean {
+export function isIsoDateString(value: any): boolean {
     return value && typeof value === 'string' && isoDateFormat.test(value);
 }
 
