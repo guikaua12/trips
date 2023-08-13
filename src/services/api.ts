3cf04a4ecd9bd7b908f111fa2332d6850b366b7c
Approximations <datkidbosst@gmail.com>
2023-08-13 18:58:59 -0300

    default headers now are handled in the ap iobject rather than in a interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,14 +5,14 @@ export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
 });
 
+api.defaults.headers.common = {
+    'Content-Type': 'application/json',
+};
+
 api.interceptors.request.use((config) => {
     const { trips_token } = nookies.get({});
     if (trips_token && !config.headers.authorization) {
         config.headers.authorization = `Bearer ${trips_token}`;
-    }
-
-    if (config.data) {
-        config.headers['Content-Type'] = 'application/json';
     }
 
     return config;
