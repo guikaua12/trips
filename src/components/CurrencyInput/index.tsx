50f03cc1f9fb798f01914cf160c6812e6e471fe8
Approximations <datkidbosst@gmail.com>
2023-08-13 19:04:17 -0300

    hook form support for CurrencyInput & some style adjustments

--- src/components/CurrencyInput/index.tsx
+++ src/components/CurrencyInput/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ChangeEventHandler, InputHTMLAttributes, useRef, useState } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 function countDecimalPlaces(number: number) {
@@ -17,41 +17,55 @@ function limitPlaces(number: number) {
 
 interface CurrencyInputProps extends InputHTMLAttributes<HTMLInputElement> {
     wrapperClassName?: string;
+    hookFormRegister?: any;
+    error?: string | undefined | null;
 }
 
-export default function CurrencyInput({ id, className, wrapperClassName, onBlur, ...props }: CurrencyInputProps) {
-    const ref = useRef<HTMLInputElement>(null);
-
+export default function CurrencyInput({
+    id,
+    className,
+    wrapperClassName,
+    onBlur,
+    hookFormRegister,
+    error,
+    ...props
+}: CurrencyInputProps) {
     return (
-        <div
-            className={twMerge(
-                'group flex flex-row-reverse items-center gap-1 border border-lightGray rounded-md p-2 text-sm text-gray focus-within:border-purple bg-white',
-                wrapperClassName
-            )}
-        >
-            <input
-                ref={ref}
-                id={id}
+        <div className={wrapperClassName}>
+            <div
                 className={twMerge(
-                    'w-full h-full outline-none group-focus-within:text-purple [&:focus~label]:text-purple [&:not(:placeholder-shown)~label]:inline',
-                    className
+                    'group flex flex-row-reverse items-center gap-1 rounded-md bg-white p-2 text-sm text-gray outline outline-1 outline-lightGray focus-within:outline-purple',
+                    error && 'outline-red-500 hover:outline-red-500 focus:focus-within:outline-red-500'
                 )}
-                type="number"
-                onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
-                    const afterComma = countDecimalPlaces(e.target.valueAsNumber);
+            >
+                {/*{error && <FaTriangleExclamation className="min-h-[20px] min-w-[20px] text-red-500" />}*/}
+                <input
+                    id={id}
+                    className={twMerge(
+                        'h-full w-full outline-none group-focus-within:text-purple [&:focus~label]:text-purple [&:not(:placeholder-shown)~label]:inline',
+                        className
+                    )}
+                    type="number"
+                    onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
+                        const afterComma = countDecimalPlaces(e.target.valueAsNumber);
 
-                    if (afterComma > 2) {
-                        // e.target.value = e.target.valueAsNumber.toFixed(2);
-                        e.target.valueAsNumber = limitPlaces(e.target.valueAsNumber);
-                    }
+                        if (afterComma > 2) {
+                            // e.target.value = e.target.valueAsNumber.toFixed(2);
+                            e.target.valueAsNumber = limitPlaces(e.target.valueAsNumber);
+                        }
 
-                    if (onBlur) onBlur(e);
-                }}
-                {...props}
-            />
-            <label htmlFor={id} className="hidden">
-                <span>R$</span>
-            </label>
+                        if (onBlur) onBlur(e);
+                    }}
+                    {...props}
+                    {...hookFormRegister}
+                />
+
+                <label htmlFor={id} className="hidden">
+                    <span>R$</span>
+                </label>
+            </div>
+
+            {error && <span className="text-xs text-red-500">{error}</span>}
         </div>
     );
 }
