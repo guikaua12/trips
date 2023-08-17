59accef18badd9912d5282e4be63d01e39538524
Approximations <datkidbosst@gmail.com>
2023-08-16 23:47:54 -0300

    feat: implement button variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,19 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export default function Button({ className, children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonVariants {
+    Primary = 'bg-purple text-white outline-none disabled:bg-darkPurple font-medium',
+    Secondary = 'bg-transparent text-purple outline outline-1 font-semibold',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariants;
+}
+
+export default function Button({ className, children, variant = ButtonVariants.Primary, ...props }: ButtonProps) {
     return (
         <button
-            className={twMerge(
-                'outline-none rounded-md bg-purple p-2 text-white text-sm font-medium text-center flex justify-center',
-                'disabled:bg-darkPurple',
-                className
-            )}
+            className={twMerge('flex justify-center rounded-md p-2 text-center text-sm', variant, className)}
             {...props}
         >
             {children}
