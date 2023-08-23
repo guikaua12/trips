c8cc67ea137b18e86297843b52d61c79de7690ea
Approximations <datkidbosst@gmail.com>
2023-08-22 23:09:25 -0300

    pricePerDay > totalPrice

--- src/app/components/TotalPrice/index.tsx
+++ src/app/components/TotalPrice/index.tsx
@@ -10,7 +10,8 @@ function isValidDates(startDate: Date, endDate: Date): boolean {
 type TotalPriceProps = {
     startDate: Date;
     endDate: Date;
-    pricePerDay: number;
+    pricePerDay?: number;
+    totalPrice?: number;
     leftClassName?: string;
     rightClassName?: string;
 };
@@ -19,17 +20,27 @@ export default function TotalPrice({
     startDate,
     endDate,
     pricePerDay,
+    totalPrice,
     leftClassName,
     rightClassName,
 }: TotalPriceProps) {
-    const totalPrice = pricePerDay * differenceInDays(endDate, startDate);
+    let curTotalPrice = 0;
+
+    if (pricePerDay) {
+        curTotalPrice = pricePerDay * differenceInDays(endDate, startDate);
+    }
+
+    if (totalPrice) {
+        curTotalPrice = totalPrice;
+    }
+
     const isValidDate = isValidDates(startDate, endDate);
     const difference = differenceInDays(endDate, startDate);
 
     return (
         <div className="col-span-2 flex w-full justify-between text-xs font-semibold text-darkPurple">
             <p className={leftClassName}>{isValidDate ? `Total (${difference} noites):` : 'Total:'}</p>
-            <p className={rightClassName}>{isValidDate ? `R$ ${totalPrice.toFixed(2)}` : 'R$0.00'}</p>
+            <p className={rightClassName}>{isValidDate ? `R$ ${curTotalPrice.toFixed(2)}` : 'R$0.00'}</p>
         </div>
     );
 }
