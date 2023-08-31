1e7c16edf16f848776cd4ee1f2ed2368d63b80cb
Approximations <datkidbosst@gmail.com>
2023-08-31 13:08:28 -0300

    added correct Order By values

--- src/app/components/OrderBy/index.tsx
+++ src/app/components/OrderBy/index.tsx
@@ -8,10 +8,10 @@ interface Props extends ComponentProps<'div'> {
 export default function OrderBy({ className, handleChange, ...props }: Props) {
     return (
         <SelectToggle.Select className="z-50" handleChange={handleChange} placeholder="Ordenar por" {...props}>
-            <SelectToggle.Option value={10}>Dez</SelectToggle.Option>
-            <SelectToggle.Option value={20} selected>
-                Vinte
+            <SelectToggle.Option value="createdAt" selected>
+                Data de criação
             </SelectToggle.Option>
+            <SelectToggle.Option value="totalPaid">Preço</SelectToggle.Option>
         </SelectToggle.Select>
     );
 }
