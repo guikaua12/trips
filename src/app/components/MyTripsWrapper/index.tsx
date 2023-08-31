aa13314e02f2468decd4e561524d00a839ccd5d2
Approximations <datkidbosst@gmail.com>
2023-08-31 13:08:09 -0300

    refactored `MyTripsWrapper` component

--- src/app/components/MyTripsWrapper/index.tsx
+++ src/app/components/MyTripsWrapper/index.tsx
@@ -13,35 +13,49 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import CircleLoading from '@/components/CircleLoading';
 import OrderBy from '@/app/components/OrderBy';
 import MyTripsWrapperSkeleton from '@/app/components/MyTripsWrapperSkeleton';
+import { SelectItem } from '@/components/SelectToggle/Select';
 
 type MyTripsWrapperProps = {
     response: GetAllTripReservationsResponseType;
 };
 
+type State = {
+    page: number;
+    response: GetAllTripReservationsResponseType;
+    items: TripReservation[];
+    loading: boolean;
+    sortBy?: string | null;
+    sortDir?: string | null;
+};
+
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default function MyTripsWrapper({ response: defaultResponse }: MyTripsWrapperProps) {
-    const [page, setPage] = useState(1);
-    const [response, setResponse] = useState<GetAllTripReservationsResponseType>(defaultResponse);
-    // const [sortBy, setSortBy] = useState<SelectItem | null>(null);
-    // const [sortDir, setSortDir] = useState<SelectItem | null>(null);
-    const [items, setItems] = useState<TripReservation[]>(defaultResponse.tripReservations || []);
-    const [loading, setLoading] = useState(false);
+    const [state, setState] = useState<State>({
+        page: 1,
+        response: defaultResponse,
+        items: defaultResponse.tripReservations || [],
+        loading: false,
+        sortBy: undefined,
+        sortDir: undefined,
+    });
 
     const alert = useCallback((message: string, type: TypeOptions) => toast(message, { type: type }), []);
 
     const next = async () => {
         console.log('next');
+        const nextPage = state.page + 1;
+        console.log(nextPage);
+        const response = await getAllTripReservations({ page_start: nextPage });
 
-        const nextPage = page + 1;
-        setPage((lastPage) => nextPage);
+        if (!response.tripReservations) return;
 
-        const response = await getAllTripReservations({ page: nextPage });
-        setResponse(response);
-
-        if (response.tripReservations) {
-            setItems((prevState) => [...prevState, ...response.tripReservations!]);
-        }
+        setState((prevState) => ({
+            ...prevState,
+            page: nextPage,
+            response,
+            items: [...prevState.items, ...response.tripReservations!] || [],
+        }));
     };
 
     const handleCancelClick = async (tripReservation: TripReservation) => {
@@ -55,36 +69,34 @@ export default function MyTripsWrapper({ response: defaultResponse }: MyTripsWra
 
         if (!response.tripReservation) return;
 
-        // setItems((prevState) => [...prevState, tripReservation]);
-        setItems((prevState) =>
-            prevState.map((item) => (item.id === tripReservation.id ? response.tripReservation! : item))
-        );
+        // // setItems((prevState) => [...prevState, tripReservation]);
+        // setItems((prevState) =>
+        //     prevState.map((item) => (item.id === tripReservation.id ? response.tripReservation! : item))
+        // );
 
         alert('Viagem cancelada com sucesso!', 'success');
     };
 
-    // const handleSortByChange = async (item: SelectItem) => {
-    //     // setSortBy(item);
-    //
-    //     try {
-    //         setLoading(true);
-    //         await delay(650);
-    //         const response = await getAllTripReservations({
-    //             sort_by: item.value,
-    //             sort_dir: sortDir?.value,
-    //             page: 1,
-    //         });
-    //
-    //         setResponse(response);
-    //
-    //         if (response.tripReservations) {
-    //             setItems(response.tripReservations);
-    //         }
-    //     } catch (e) {
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // };
+    const handleSortByChange = async (item: SelectItem) => {
+        try {
+            setState((prevState) => ({ ...prevState, sortBy: item.value, loading: true }));
+            await delay(650);
+            const response = await getAllTripReservations({
+                sort_by: item.value,
+                sort_dir: state.sortDir || undefined,
+                page_start: 1,
+            });
+
+            setState((prevState) => ({
+                ...prevState,
+                response,
+                items: response.tripReservations || [],
+            }));
+        } catch (e) {
+        } finally {
+            setState((prevState) => ({ ...prevState, loading: false }));
+        }
+    };
 
     // const handleSortDirChange = async (item: SelectItem) => {
     //     setSortDir(item);
@@ -112,20 +124,15 @@ export default function MyTripsWrapper({ response: defaultResponse }: MyTripsWra
     return (
         <>
             <div className="mb-4 flex justify-end gap-1">
-                <OrderBy
-                    className="mb-4"
-                    handleChange={(item) => {
-                        console.log(item);
-                    }}
-                />
+                <OrderBy className="mb-4" handleChange={handleSortByChange} />
                 {/*<OrderDir className="mb-4" selected={sortDir} handleChange={handleSortDirChange} />*/}
             </div>
-            {!loading && items.length && (
+            {!state.loading && state.items.length && (
                 <InfiniteScroll
                     className="flex flex-col gap-5"
-                    dataLength={items.length}
+                    dataLength={state.items.length}
                     next={next}
-                    hasMore={page - 1 < response.pages!}
+                    hasMore={state.page - 1 < state.response.pages!}
                     loader={
                         <div className="flex w-full items-center justify-center">
                             <CircleLoading />
@@ -133,7 +140,7 @@ export default function MyTripsWrapper({ response: defaultResponse }: MyTripsWra
                     }
                     endMessage={<p className="text-center">Não há mais nada para carregar.</p>}
                 >
-                    {items.map((tripReservation) => (
+                    {state.items.map((tripReservation) => (
                         <MyTrip
                             key={tripReservation.id}
                             tripReservation={tripReservation}
@@ -143,9 +150,9 @@ export default function MyTripsWrapper({ response: defaultResponse }: MyTripsWra
                 </InfiniteScroll>
             )}
 
-            {!loading && !items.length && <p className="text-center">Nenhuma hospedagem encontrada!</p>}
+            {!state.loading && !state.items.length && <p className="text-center">Nenhuma hospedagem encontrada!</p>}
 
-            {loading && <MyTripsWrapperSkeleton quantity={2} />}
+            {state.loading && <MyTripsWrapperSkeleton quantity={2} />}
         </>
     );
 }
