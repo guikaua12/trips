9e7957cc50c74a11316396fd2da2058683b49791
Approximations <datkidbosst@gmail.com>
2023-09-06 16:23:01 -0300

    remove unused `twMerge` from Option.tsx

--- src/components/SelectToggle/Option.tsx
+++ src/components/SelectToggle/Option.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from 'react';
-import { twMerge } from 'tailwind-merge';
 
 type Props = {
     children?: ReactNode;
@@ -10,7 +9,7 @@ type Props = {
 export default function Option({ children, value, selected = false }: Props) {
     return (
         <button
-            className={twMerge('w-full px-1 py-1 text-start hover:bg-zinc-100')}
+            className="w-full px-1 py-1 text-start hover:bg-zinc-100"
             data-select-item={true}
             data-label={children}
             data-value={value}
